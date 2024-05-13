9cce8359c385d6b822db15665b4dd74383af2e42
Priyadharsini-123 <priyadharsinib444@gmail.com>
2024-05-13 19:18:58 +0530

    some ui changesss

--- src/Section/Header.js
+++ src/Section/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Logo from '../images/logo.png'
+import Tow from '../images/logo.png'
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0 navcustom sticky-top">
             <a href="/" className="navbar-brand p-0">
-                <h1 className="m-0"><img src={Logo} alt="Logo" /></h1>
+                <h1 className="m-0"> <img src={Tow} alt="Logo" /></h1>
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span className="fa fa-bars"></span>
